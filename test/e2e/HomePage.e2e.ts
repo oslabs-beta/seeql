4cfa782944744c107ec192ed841c43d4c01f63de
Kate Matthews <katesmatthews@gmail.com>
2019-06-08 14:05:52 -0400

    Comment out boilerplate tests

--- test/e2e/HomePage.e2e.ts
+++ test/e2e/HomePage.e2e.ts
@@ -32,71 +32,71 @@ test(
   assertNoConsoleErrors
 );
 
-test('should to Counter with click "to Counter" link', async t => {
-  await t
-    .click('[data-tid=container] > a')
-    .expect(getCounterText())
-    .eql('0');
-});
+// test('should to Counter with click "to Counter" link', async t => {
+//   await t
+//     .click('[data-tid=container] > a')
+//     .expect(getCounterText())
+//     .eql('0');
+// });
 
-test('should navgiate to /counter', async t => {
-  await waitForReact();
-  await t
-    .click(
-      ReactSelector('Link').withProps({
-        to: '/counter'
-      })
-    )
-    .expect(getPageUrl())
-    .contains('/counter');
-});
+// test('should navgiate to /counter', async t => {
+//   await waitForReact();
+//   await t
+//     .click(
+//       ReactSelector('Link').withProps({
+//         to: '/counter'
+//       })
+//     )
+//     .expect(getPageUrl())
+//     .contains('/counter');
+// });
 
-fixture`Counter Tests`
-  .page('../../app/app.html')
-  .beforeEach(clickToCounterLink)
-  .afterEach(assertNoConsoleErrors);
+// fixture`Counter Tests`
+//   .page('../../app/app.html')
+//   .beforeEach(clickToCounterLink)
+//   .afterEach(assertNoConsoleErrors);
 
-test('should display updated count after increment button click', async t => {
-  await t
-    .click(incrementButton)
-    .expect(getCounterText())
-    .eql('1');
-});
+// test('should display updated count after increment button click', async t => {
+//   await t
+//     .click(incrementButton)
+//     .expect(getCounterText())
+//     .eql('1');
+// });
 
-test('should display updated count after descrement button click', async t => {
-  await t
-    .click(decrementButton)
-    .expect(getCounterText())
-    .eql('-1');
-});
+// test('should display updated count after descrement button click', async t => {
+//   await t
+//     .click(decrementButton)
+//     .expect(getCounterText())
+//     .eql('-1');
+// });
 
-test('should not change if even and if odd button clicked', async t => {
-  await t
-    .click(oddButton)
-    .expect(getCounterText())
-    .eql('0');
-});
+// test('should not change if even and if odd button clicked', async t => {
+//   await t
+//     .click(oddButton)
+//     .expect(getCounterText())
+//     .eql('0');
+// });
 
-test('should change if odd and if odd button clicked', async t => {
-  await t
-    .click(incrementButton)
-    .click(oddButton)
-    .expect(getCounterText())
-    .eql('2');
-});
+// test('should change if odd and if odd button clicked', async t => {
+//   await t
+//     .click(incrementButton)
+//     .click(oddButton)
+//     .expect(getCounterText())
+//     .eql('2');
+// });
 
-test('should change if async button clicked and a second later', async t => {
-  await t
-    .click(asyncButton)
-    .expect(getCounterText())
-    .eql('0')
-    .expect(getCounterText())
-    .eql('1');
-});
+// test('should change if async button clicked and a second later', async t => {
+//   await t
+//     .click(asyncButton)
+//     .expect(getCounterText())
+//     .eql('0')
+//     .expect(getCounterText())
+//     .eql('1');
+// });
 
-test('should back to home if back button clicked', async t => {
-  await t
-    .click('[data-tid="backButton"] > a')
-    .expect(Selector('[data-tid="container"]').visible)
-    .ok();
-});
+// test('should back to home if back button clicked', async t => {
+//   await t
+//     .click('[data-tid="backButton"] > a')
+//     .expect(Selector('[data-tid="container"]').visible)
+//     .ok();
+// });
