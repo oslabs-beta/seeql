27e1096fab74f6b97ce25a2d60b4971012c570e7
fefe <saylestyler@gmail.com>
2019-06-09 17:25:11 -0400

    * adds deps for typescript+eslint working alongside prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,24 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
   parserOptions: {
-    sourceType: 'module',
-    allowImportExportEverywhere: true
-  },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  env: {
-    browser: true,
-    node: true
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
   },
   rules: {
-    'arrow-parens': ['off'],
-    'compat/compat': 'error',
-    'consistent-return': 'off',
-    'comma-dangle': 'off',
-    'generator-star-spacing': 'off',
-    'import/no-unresolved': 'error',
-    'import/no-extraneous-dependencies': 'off',
-    'jsx-a11y/anchor-is-valid': 'off',
-    'no-console': 'off',
-    'no-use-before-define': 'off',
-    'no-multi-assign': 'off',
-    'promise/param-names': 'error',
-    'promise/always-return': 'error',
-    'promise/catch-or-return': 'error',
-    'promise/no-native': 'off',
-    'react/sort-comp': [
-      'error',
-      {
-        order: [
-          'type-annotations',
-          'static-methods',
-          'lifecycle',
-          'everything-else',
-          'render'
-        ]
-      }
-    ],
-    'react/jsx-no-bind': 'off',
-    'react/jsx-filename-extension': [
-      'error',
-      {
-        extensions: ['.js', '.jsx']
-      }
-    ],
-    'react/prefer-stateless-function': 'off'
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
-  plugins: ['import', 'promise', 'compat', 'react'],
   settings: {
-    'import/resolver': {
-      webpack: {
-        config: 'configs/webpack.config.eslint.js'
-      }
+    react: {
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
     }
   }
 };
