37f29627124aaf945d8d9d3fae4be9560afc8cfe
Tyler Sayles <saylestyler@gmail.com>
2019-06-10 10:42:42 -0400

    Ts refactor db from login (#9)
    * add db class
    * move db logic out of `Login.tsx`
    * add typings from `pg`, `styled-components`
    * imports `pg` to `Login.tsx` to make initial connection

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -29,9 +29,10 @@ const URIInput = styled.textarea`
   height: 100px;
   border-radius: 3px;
   border: ${ (props) =>
-    props.requiredErr
-      ? '1px solid #ca333e'
-      : '1px solid lightgrey'};
+		props.requiredErr
+			? '1px solid #ca333e'
+			: '1px solid lightgrey'
+	}
   overflow: wrap;
   resize: none;
   transition: 0.3s;
@@ -72,178 +73,81 @@ const RequiredWarning = styled.span`
 `
 
 const Login = () => {
-  const [URI, setURI] = useState('');
-  const [isSSL, setSSL] = useState(false);
-  const [requiredError, setRequiredError] = useState(false);
-  const [connectionError, setConnectionError] = useState(false);
-  const [isLoading, setLoading] = useState(false);
-  const [redirectToHome, setRedirectToHome] = useState(false);
+	const [URI, setURI] = useState('');
+	const [isSSL, setSSL] = useState(false);
+	const [requiredError, setRequiredError] = useState(false);
+	const [connectionError, setConnectionError] = useState(false);
+	const [isLoading, setLoading] = useState(false);
+	const [redirectToHome, setRedirectToHome] = useState(false);
 
-  const sendLoginURI = (): void => {
+	const sendLoginURI = (): void => {
+		let updatedURI = URI;
+		if (isSSL) updatedURI += '?ssl=true';
+		if (!URI) setRequiredError(true);
+		else {
+			setLoading(true);
+			const client = new Client(updatedURI)
+			client.connect((err: string, res: string) => {
+				if (err) {
+					setConnectionError(true);
+					setLoading(false);
+				} else {
+					setRedirectToHome(true);
+					setConnectionError(false);
+				}
+			})
+		}
+	};
 
-    let updatedURI = URI;
-    if (isSSL) updatedURI += '?ssl=true';
+	const captureURI = (e): void => {
+		const sanitizedURI = e.target.value.replace(/\s+/g, "");
+		setURI(sanitizedURI);
+		if (requiredError) setRequiredError(false);
+	};
 
-    const client = new Client(updatedURI)
+	const redirectHome = () => {
+		if (redirectToHome) return <Redirect to="/homepage" />;
+	};
 
-    client.connect((err: string, res: string) => {
-      if (err) {
-        console.log(err, 'err conecting')
-      } else {
-        console.log('we connected')
-      }
-    })
+	return (
+		<LoginContainer>
+			{ connectionError
+				&& <ConnectionErrorMessage>Unable to connect to the database. Please try again.</ConnectionErrorMessage>
+			}
 
-    const getTables = () => {
-      return new Promise((resolve, reject) => {
-        client.query(`SELECT table_name
-                      FROM information_schema.tables
-                      WHERE table_schema='public'
-                      AND table_type='BASE TABLE'`,
-          (err: string, result: string) => {
-            if (err) reject(err);
-            resolve(result)
-          });
-      })
-    }
+			<InputLabel>
+				URI Connection String
+			</InputLabel>
 
-    const getForeignKeys = (tableName: string) => {
-      return new Promise((resolve, reject) => {
-        client.query(`SELECT tc.table_schema,
-                              tc.constraint_name,
-                              tc.table_name,
-                              kcu.column_name,
-                              ccu.table_schema AS foreign_table_schema,
-                              ccu.table_name AS foreign_table_name,
-                              ccu.column_name AS foreign_column_name
-                       FROM information_schema.table_constraints AS tc
-                       JOIN information_schema.key_column_usage AS kcu
-                       ON tc.constraint_name = kcu.constraint_name
-                       AND tc.table_schema = kcu.table_schema
-                       JOIN information_schema.constraint_column_usage AS ccu
-                       ON ccu.constraint_name = tc.constraint_name
-                       AND ccu.table_schema = tc.table_schema
-                       WHERE tc.constraint_type = 'FOREIGN KEY'
-                       AND tc.table_name = '${tableName}'`,
-          (err: string, result: string) => {
-            if (err) reject(err);
-            resolve(result)
-          });
-      });
-    }
+			<URIInput
+				requiredErr={requiredError}
+				onChange={captureURI}
+				placeholder="Enter your URI connection string..."
+			>
+			</URIInput>
 
-    const getColumns = (tableName: string) => {
-      return new Promise((resolve, reject) => {
-        client.query(`SELECT COLUMN_NAME AS ColumnName,
-                             DATA_TYPE AS DataType,
-                             CHARACTER_MAXIMUM_LENGTH AS CharacterLength,
-                             COLUMN_DEFAULT AS DefaultValue
-                      FROM INFORMATION_SCHEMA.COLUMNS
-                      WHERE TABLE_NAME = '${tableName}'`,
-          (err: string, result: string) => {
-            if (err) reject(err);
-            resolve(result)
-          });
-      });
-    }
+			{requiredError
+				&& <RequiredWarning>This field is required</RequiredWarning>
+			}
 
-    const getPrimaryKey = (tableName: string) => {
-      return new Promise((resolve, reject) => {
-        client.query(`SELECT column_name
-                      FROM pg_constraint, information_schema.constraint_column_usage
-                      WHERE contype = 'p'
-                      AND information_schema.constraint_column_usage.table_name = '${tableName}'
-                      AND pg_constraint.conname = information_schema.constraint_column_usage.constraint_name`,
-          (err: string, result: string) => {
-            if (err) reject(err);
-            resolve(result)
-          });
-      });
-    }
+			<ToggleSSL>
+				<input type="checkbox" onChange={(e) => setSSL(e.target.checked)} />
+				<label>ssl?</label>
+			</ToggleSSL>
 
-    async function composeTableData() {
-      let tablesArr = []
-      var tableNames: any = null
-      tableNames = await getTables()
-
-      for (let table of tableNames.rows) {
-        table.primaryKey = await getPrimaryKey(table.table_name)
-        table.foreignKey = await getForeignKeys(table.table_name)
-        table.columns = await getColumns(table.table_name)
-
-        tablesArr.push(table)
-      }
-
-      return new Promise((resolve, reject) => {
-        resolve(tablesArr)
-      })
-    }
-    composeTableData().then(r => console.log('table data #TODO: render when component is decided', r))
-
-    if (!URI) setRequiredError(true);
-    else {
-      setLoading(true);
-
-      // #TODO: handle errors - currently redirects to home automatically
-      if (true) {
-        setRedirectToHome(true);
-        setConnectionError(false);
-      }
-      else {
-        setConnectionError(true);
-        setLoading(false);
-      };
-    };
-  };
-
-  const captureURI = (e): void => {
-    const sanitizedURI = e.target.value.replace(/\s+/g, "");
-    setURI(sanitizedURI);
-    if (requiredError) setRequiredError(false);
-  };
-
-  const redirectHome = () => {
-    if (redirectToHome) return <Redirect to="/homepage" />;
-  };
-
-  return (
-    <LoginContainer>
-      {
-        connectionError
-        && <ConnectionErrorMessage>Unable to connect to the database. Please try again.</ConnectionErrorMessage>
-      }
-
-      <InputLabel>URI Connection String</InputLabel>
-
-      <URIInput
-        requiredErr={requiredError}
-        onChange={captureURI}
-        placeholder="Enter your URI connection string..."
-      >
-      </URIInput>
-
-      {requiredError
-        && <RequiredWarning>This field is required</RequiredWarning>
-      }
-
-      <ToggleSSL>
-        <input type="checkbox" onChange={(e) => setSSL(e.target.checked)} />
-        <label>ssl?</label>
-      </ToggleSSL>
-
-      {!isLoading
-        && <LoginBtn onClick={sendLoginURI}>Login</LoginBtn>
-      }
-      {isLoading
-        && <LoginBtn
-          disabled
-        >Loading...
+			{!isLoading
+				&& <LoginBtn onClick={sendLoginURI}>Login</LoginBtn>
+			}
+			{isLoading
+				&& <LoginBtn
+					disabled
+				>Loading...
         </LoginBtn>
-      }
+			}
 
-      {redirectHome()}
-    </LoginContainer>
-  );
-}
+			{redirectHome()}
+		</LoginContainer>
+	);
+};
 
 export default Login;
