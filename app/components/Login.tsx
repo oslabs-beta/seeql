f9371e26856622a4fb985bb75dd5a6044cfe6dd8
Tyler <saylestyler@gmail.com>
2019-06-11 17:04:40 -0400

    add bug to db; add db urls; delete readme

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,41 +1,40 @@
-import * as React from 'react';
-import {useState} from 'react';
-import styled from 'styled-components';
-import {Redirect} from 'react-router-dom';
-import {Client} from 'pg';
+import * as React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import { Redirect } from "react-router-dom";
+import { Client } from "pg";
 import composeTableData from "../db";
 
-
 interface URIInputProps {
-	readonly requiredErr: boolean;
-  };
-  
+  readonly requiredErr: boolean;
+}
+
 const URIInput = styled.textarea<URIInputProps>`
-	width: 200px;
-	height: 100px;
-	border-radius: 3px;
-	font-family: 'Poppins', sans-serif;
-	letter-spacing: 2px;
-	resize: none;
-	padding: 10px;
-	border: ${props => props.requiredErr ? '1px solid #ca333e' : '1px solid lightgrey'};
+  width: 200px;
+  height: 100px;
+  border-radius: 3px;
+  font-family: "Poppins", sans-serif;
+  letter-spacing: 2px;
+  resize: none;
+  padding: 10px;
+  border: ${props =>
+    props.requiredErr ? "1px solid #ca333e" : "1px solid lightgrey"};
 
-	:focus {
-		outline: none;
-	}
-`
-
+  :focus {
+    outline: none;
+  }
+`;
 
 const ToggleSSL = styled.div`
   display: flex;
   padding: 5px 20px;
   margin: 10px;
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   display: flex;
   color: black;
   align-items: center;
   font-size: 80%;
-`
+`;
 
 const LoginContainer = styled.div`
   background-color: #e8ecf1;
@@ -45,19 +44,19 @@ const LoginContainer = styled.div`
   justify-content: center;
   padding: 50px;
   height: 100vh;
-`
+`;
 
 const LoginBtn = styled.button`
   padding: 5px 20px;
   margin: 10px;
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   display: flex;
-`
+`;
 
 const InputLabel = styled.span`
   color: black;
-  font-family: 'Poppins', sans-serif;
-`
+  font-family: "Poppins", sans-serif;
+`;
 
 const ConnectionErrorMessage = styled.div`
   background-color: #f1c7ca;
@@ -66,100 +65,110 @@ const ConnectionErrorMessage = styled.div`
   padding: 10px;
   margin: 5px;
   font-size: 80%;
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   transition: 0.5s;
-`
+`;
 
 const RequiredWarning = styled.span`
   color: #ca333e;
   font-size: 60%;
-`
+`;
 
 const Login = () => {
+  const [tableData, setTableData] = useState([]);
+  const [URI, setURI] = useState("");
+  const [isSSL, setSSL] = useState(false);
+  const [requiredError, setRequiredError] = useState(false);
+  const [connectionError, setConnectionError] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [redirectToHome, setRedirectToHome] = useState(false);
 
-    const [tableData, setTableData] = useState([]);
-	const [URI, setURI] = useState('');
-	const [isSSL, setSSL] = useState(false);
-	const [requiredError, setRequiredError] = useState(false);
-	const [connectionError, setConnectionError] = useState(false);
-	const [isLoading, setLoading] = useState(false);
-	const [redirectToHome, setRedirectToHome] = useState(false);
-
-	const sendLoginURI = (): void => {
-		let updatedURI = URI;
-		if (isSSL) updatedURI += '?ssl=true';
-		if (!URI) setRequiredError(true);
-		else {
-			setLoading(true);
-			const client = new Client(updatedURI)
-			client.connect((err: Error) => {
-				if (err) {
-					setConnectionError(true);
-					setLoading(false);
-				} else {
+  const sendLoginURI = (): void => {
+    let updatedURI = URI;
+    if (isSSL) updatedURI += "?ssl=true";
+    if (!URI) setRequiredError(true);
+    else {
+      setLoading(true);
+      const client = new Client(updatedURI);
+      client.connect((err: Error) => {
+        if (err) {
+          setConnectionError(true);
+          setLoading(false);
+        } else {
           composeTableData(client)
             .then(tables => {
-			  setConnectionError(false);
-			  setTableData(tables);
-			  setLoading(false);
+              setConnectionError(false);
+              setTableData(tables);
+              setLoading(false);
               setRedirectToHome(true);
             })
-            .catch((err: any) => console.log('composeTableData error:', err))
-				}
-			})
-		}
-	};
+            .catch((err: any) => console.log("composeTableData error:", err));
+        }
+      });
+    }
+  };
 
-	const captureURI = (e): void => {
-		const sanitizedURI = e.target.value.replace(/\s+/g, "");
-		setURI(sanitizedURI);
-		if (requiredError) setRequiredError(false);
-	};
+  const captureURI = (e): void => {
+    const sanitizedURI = e.target.value.replace(/\s+/g, "");
+    setURI(sanitizedURI);
+    if (requiredError) setRequiredError(false);
+  };
 
-	const redirectHome = () => {
-		if (redirectToHome) return <Redirect to={{ pathname: "/homepage", state: { tables: tableData } } } />;
-	};
+  const redirectHome = () => {
+    if (redirectToHome)
+      return (
+        <Redirect
+          to={{ pathname: "/homepage", state: { tables: tableData } }}
+        />
+      );
+  };
 
-	return (
-		<LoginContainer>
-			{ connectionError
-				&& <ConnectionErrorMessage>Unable to connect to the database. Please try again.</ConnectionErrorMessage>
-			}
+  return (
+    <LoginContainer>
+      {connectionError && (
+        <ConnectionErrorMessage>
+          Unable to connect to the database. Please try again.
+        </ConnectionErrorMessage>
+      )}
 
-			<InputLabel>
-				URI Connection String
-				postgres://ltdnkwnbccooem:64ad308e565b39cc070194f7fa621ae0e925339be5a1c69480ff2a4462eab4c4@ec2-54-163-226-238.compute-1.amazonaws.com:5432/ddsu160rb5t7vq
-			</InputLabel>
+      <InputLabel>
+				<em>
+        CS_DEMO_DB
+				</em>
+        <br />
+        <br />
+postgres://godugvmyduvduy:3b89454dd6a4090ac4a5574a00a2e13393dda232f258b3a6033c4ac4d24858ff@ec2-50-19-127-115.compute-1.amazonaws.com:5432/d16hrptvvsaq3f
+        <br />
+        <br />
+				<em>
+        MAGNOLIA:
+				</em>
+        <br />
+        <br />
+        postgres://ltdnkwnbccooem:64ad308e565b39cc070194f7fa621ae0e925339be5a1c69480ff2a4462eab4c4@ec2-54-163-226-238.compute-1.amazonaws.com:5432/ddsu160rb5t7vq
+      </InputLabel>
 
-			<URIInput 
-				requiredErr={requiredError}
-				onChange={captureURI}
-				placeholder="Enter your URI connection string..."
-			>
-			</URIInput>
+      <URIInput
+        requiredErr={requiredError}
+        onChange={captureURI}
+        placeholder="Enter your URI connection string..."
+      ></URIInput>
 
-			{requiredError
-				&& <RequiredWarning>This field is required</RequiredWarning>
-			}
+      {requiredError && (
+        <RequiredWarning>This field is required</RequiredWarning>
+      )}
 
-			<ToggleSSL>
-				<input type="checkbox" onChange={(e) => setSSL(e.target.checked)} />
-				<label>ssl?</label>
-			</ToggleSSL>
+      <ToggleSSL>
+        <input type="checkbox" onChange={e => setSSL(e.target.checked)} />
+        <label>ssl?</label>
+      </ToggleSSL>
 
-			{!isLoading
-				&& <LoginBtn onClick={sendLoginURI}>Login</LoginBtn>
-			}
-			{isLoading
-				&& <LoginBtn
-					disabled
-				>Loading...
-        </LoginBtn>
-			}
+      {!isLoading && <LoginBtn onClick={sendLoginURI}>Login</LoginBtn>}
+      {isLoading && <LoginBtn disabled>Loading...</LoginBtn>}
 
-			{redirectHome()}
-		</LoginContainer>
-	);
+      {redirectHome()}
+    </LoginContainer>
+  );
 };
 
 export default Login;
--- app/db.ts
+++ app/db.ts
@@ -1,19 +1,22 @@
-const getTables = (client) => {
-    return new Promise((resolve, reject) => {
-        client.query(`SELECT table_name
+const getTables = client => {
+  return new Promise((resolve, reject) => {
+    client.query(
+      `SELECT table_name
                       FROM information_schema.tables
                       WHERE table_schema='public'
                       AND table_type='BASE TABLE'`,
-            (err: string, result: string) => {
-                if (err) reject(err);
-                resolve(result)
-            });
-    })
-}
+      (err: string, result: string) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
+};
 
 const getForeignKeys = (client, tableName: string) => {
-    return new Promise((resolve, reject) => {
-        client.query(`SELECT tc.table_schema,
+  return new Promise((resolve, reject) => {
+    client.query(
+      `SELECT tc.table_schema,
                          tc.constraint_name,
                          tc.table_name,
                          kcu.column_name,
@@ -29,61 +32,71 @@ const getForeignKeys = (client, tableName: string) => {
                   AND ccu.table_schema = tc.table_schema
                   WHERE tc.constraint_type = 'FOREIGN KEY'
                   AND tc.table_name = '${tableName}'`,
-            (err: string, result: any) => {
-                if (err) reject(err);
-                resolve(result.rows)
-            });
-    });
-}
+      (err: string, result: any) => {
+        if (err) reject(err);
+        resolve(result.rows);
+      }
+    );
+  });
+};
 
+// #TODO: add error handling when tables lack a primary key
+// Relational database theory dictates that every table must have a primary key.
+// This rule is not enforced by PostgreSQL, but it is usually best to follow it.
 const getColumns = (client, tableName: string) => {
-    return new Promise((resolve, reject) => {
-        client.query(`SELECT COLUMN_NAME AS ColumnName,
+  return new Promise((resolve, reject) => {
+    client.query(
+      `SELECT COLUMN_NAME AS ColumnName,
                              DATA_TYPE AS DataType,
                              CHARACTER_MAXIMUM_LENGTH AS CharacterLength,
                              COLUMN_DEFAULT AS DefaultValue
                       FROM INFORMATION_SCHEMA.COLUMNS
                       WHERE TABLE_NAME = '${tableName}'`,
-            (err: string, result: any) => {
-                if (err) reject(err);
-                resolve(result.rows)
-            });
-    });
-}
+      (err: string, result: any) => {
+        if (err)
+          // #TODO: give a msg that doesn't expose structure of database
+          reject(err);
+        resolve(result.rows);
+      }
+    );
+  });
+};
 
 const getPrimaryKey = (client, tableName: string) => {
-    return new Promise((resolve, reject) => {
-        client.query(`SELECT column_name
+  return new Promise((resolve, reject) => {
+    client.query(
+      `SELECT column_name
                       FROM pg_constraint, information_schema.constraint_column_usage
                       WHERE contype = 'p'
                       AND information_schema.constraint_column_usage.table_name = '${tableName}'
                       AND pg_constraint.conname = information_schema.constraint_column_usage.constraint_name`,
-            (err: string, result: any) => {
-                if (err) reject(err);
-                resolve(result.rows[0].column_name)
-            });
-    });
-}
+      (err: string, result: any) => {
+        if (err) reject(err);
+        resolve(result.rows[0].column_name);
+      }
+    );
+  });
+};
 
-async function composeTableData(client):Promise<any> {
-    let tablesArr = []
-    var tableNames: any = await getTables(client)
+async function composeTableData(client): Promise<any> {
+  let tablesArr = [];
+  var tableNames: any = await getTables(client);
 
-    for (let table of tableNames.rows) {
-        table.primaryKey = await getPrimaryKey(client, table.table_name)
-        table.foreignKeys = await getForeignKeys(client, table.table_name)
-        table.columns = await getColumns(client, table.table_name)
-        tablesArr.push(table)
+  for (let table of tableNames.rows) {
+    table.primaryKey = await getPrimaryKey(client, table.table_name);
+    table.foreignKeys = await getForeignKeys(client, table.table_name);
+    table.columns = await getColumns(client, table.table_name);
+    tablesArr.push(table);
+  }
+
+  return new Promise<any>((resolve: any, reject: any): any => {
+    if (tablesArr.length > 0) {
+      resolve(tablesArr);
+    } else {
+      // #TODO: add empty state trigger
+      reject(new Error("database empty"));
     }
-
-    return new Promise<any>((resolve:any, reject:any):any => {
-        if (tablesArr.length > 0) {
-            resolve(tablesArr)
-        } else {
-            // #TODO: add empty state trigger
-            reject(new Error('database empty'))
-        }
-    })
+  });
 }
 
-export default composeTableData
+export default composeTableData;
