a277a3f38f3aa1567cefd539957a0a6a3e4f56a2
Alice <alice.sky.wong@gmail.com>
2019-06-14 09:36:15 -0400

    Add actiontypes file for constant list of action types

--- app/reducers/ChangePinnedStatus.tsx
+++ app/reducers/ChangePinnedStatus.tsx
@@ -1,10 +1,12 @@
+import * as types from '../constants/actionTypes';
+
 export default function changePinnedStatus(state, action) {
     let pinnedItems = state.slice();
     switch (action.type) {
-      case 'REMOVE_FROM_PINNED':
+      case types.REMOVE_FROM_PINNED:
         let newPinnedItems = state.filter(table => table !== action.payload.tablename)
         return newPinnedItems;
-      case 'ADD_TO_PINNED':
+      case types.ADD_TO_PINNED:
         pinnedItems.push(action.payload.tablename)
         return pinnedItems;
       default:
