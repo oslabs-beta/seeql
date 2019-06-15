eb87cacd353f6cb3d3a94c11380fa46926b4232f
Tyler <saylestyler@gmail.com>
2019-06-15 14:16:18 -0400

    delete Root.tsx and nest in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { AppContainer } from "react-hot-loader";
-import Root from "./containers/Root";
 import "./app.global.css";
 import ThemeContext from "./contexts/themeContext";
 import themes from "./themes/themes";
+import Routes from "./Routes";
+import { BrowserRouter } from "react-router-dom";
 
 render(
   <ThemeContext.Provider value={themes}>
     <AppContainer>
-      <Root />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
     </AppContainer>
   </ThemeContext.Provider>,
   document.getElementById("root")
