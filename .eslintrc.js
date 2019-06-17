536ff9ca973ffb4e4034a20f62c38618ab43bfaa
Tyler <saylestyler@gmail.com>
2019-06-16 21:14:48 -0400

    adds rules from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
     extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
         "react-app",
         "plugin:prettier/recommended"
