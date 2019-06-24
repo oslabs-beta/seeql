4c0aadc683fce091ff30f11b6c4b78b6c93f7a79
Tyler Sayles <saylestyler@gmail.com>
2019-06-24 11:22:42 -0400

    save user settings [backend] (#42)
    * Add appDb class for persisting data
    * Save user settings on a per-OS basis (what electron returns from app.getPath('userData')
    * Does not implement any presentational elements
    * Fix main dev
    * Update main.dev.ts

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -1,28 +1,28 @@
-/* eslint global-require: off */
-
-/**
- * This module executes inside of electron's main process. You can start
- * electron renderer process from here and communicate with the other processes
- * through IPC.
- *
- * When running `yarn build` or `yarn build-main`, this file is compiled to
- * `./app/main.prod.js` using webpack. This gives us some performance wins.
- */
 import { app, BrowserWindow, ipcMain } from 'electron';
-import { autoUpdater } from 'electron-updater';
-import log from 'electron-log';
 import MenuBuilder from './menu';
+import AppDb from './appDb';
 
-export default class AppUpdater {
-  constructor() {
-    log.transports.file.level = 'info';
-    autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
-  }
-}
+// import { autoUpdater } from 'electron-updater';
+// #TODO: make update flow (this class is currently has 0 references)
+// export default class AppUpdater {
+//   public constructor() {
+//     log.transports.file.level = 'info';
+//     autoUpdater.logger = log;
+//     autoUpdater.checkForUpdatesAndNotify();
+//   }
+// }
 
 let mainWindow = null;
 let dbProcess = null;
+
+const defaults = {
+  configName: 'user-data',
+  width: 700, // will be the default window size if they haven't resized ever
+  height: 850,
+  savedConnections: [],
+  theme: 'default'
+};
+const appDb = new AppDb(defaults);
 
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
@@ -47,18 +47,6 @@ const installExtensions = async () => {
   ).catch(console.log);
 };
 
-/**
- * Add event listeners...
- */
-
-app.on('window-all-closed', () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
 app.on('ready', async () => {
   if (
     process.env.NODE_ENV === 'development' ||
@@ -68,30 +56,33 @@ app.on('ready', async () => {
   }
 
   mainWindow = new BrowserWindow({
-    show: false,
-    width: 1024,
-    height: 728,
+    show: true,
+    width: appDb.get('width') || defaults.width,
+    height: appDb.get('height') || defaults.height,
+    'min-width': 500,
+    'min-height': 300,
     titleBarStyle: 'hiddenInset'
   });
 
   mainWindow.loadURL(`file://${__dirname}/app.html`);
+  //https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
+  mainWindow.webContents.on('ready-to-show', () => {
+    if (!mainWindow) throw new Error('"mainWindow" is not defined');
 
-  // @TODO: Use 'ready-to-show' event
-  //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
-  mainWindow.webContents.on('did-finish-load', () => {
-    if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
-    }
     if (process.env.START_MINIMIZED) {
       mainWindow.minimize();
     } else {
+      // #TODO: decide do we show the app window
+
+      // const connStrs = appDb.get('savedConnections');
+      // mainWindow.webContents.send('saved-connections', connStrs);
+
+      // const userTheme = appDb.get('userThemePreference');
+      // mainWindow.webContents.send('user-theme-preference', userTheme);
+
       mainWindow.show();
       mainWindow.focus();
     }
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
   });
 
   const menuBuilder = new MenuBuilder(mainWindow);
@@ -100,16 +91,33 @@ app.on('ready', async () => {
   dbProcess = new BrowserWindow({ show: false });
   dbProcess.loadURL(`file://${__dirname}/dbProcess.html`);
 
+  // sent * from * the renderer process when the user selects "remember me"
+  // savedConnStr = { name: "name for connStr", uri: 'postgres://etc' }
+  ipcMain.on('remember-connection', (_event: Event, savedConnStr: any) => {
+    appDb.push('connStr', savedConnStr);
+  });
+
+  // saves queries to display in side-panel @ some point
+  ipcMain.on('query-to-main', (_event: void, query: string) => {
+    dbProcess.webContents.send('query-to-db', query);
+    appDb.push('userQueryHistory', query);
+  });
+
+  // saves users theme if they select one which isn't default
+  ipcMain.on('user-theme-selected', (_event: Event, theme: string) => {
+    appDb.set('theme', theme);
+  });
+
   // Listening from homepage, to send to database
-  ipcMain.on('uri-to-main', (_event, uri) => {
+  ipcMain.on('uri-to-main', (_event: void, uri: string) => {
     dbProcess.webContents.send('uri-to-db', uri);
   });
 
-  ipcMain.on('query-to-main', (_event, query) => {
+  ipcMain.on('query-to-main', (_event: void, query: string) => {
     dbProcess.webContents.send('query-to-db', query);
   });
 
-  ipcMain.on('logout-to-main', (_event, message) => {
+  ipcMain.on('logout-to-main', (_event: void, message: string) => {
     dbProcess.webContents.send('logout-to-db', message);
   });
 
@@ -118,23 +126,61 @@ app.on('ready', async () => {
   });
 
   // Listening from database, to send to homepage
-  ipcMain.on('database-tables-to-main', (_event, databaseTables) => {
-    mainWindow.webContents.send('tabledata-to-login', databaseTables);
-  });
+  ipcMain.on(
+    'database-tables-to-main',
+    (_event: void, databaseTables: any[]) => {
+      mainWindow.webContents.send('tabledata-to-login', databaseTables);
+      // we could save/cache this data but for now let's not
+    }
+  );
 
-  ipcMain.on('db-connection-error', (_event, err) => {
+  ipcMain.on('db-connection-error', (_event: void, err: Error) => {
     mainWindow.webContents.send('db-connection-error', err);
+    appDb.set('connectionError', err); // could be used in our crash reporting tools
   });
 
-  ipcMain.on('query-result-to-main', (_event, messagePayload) => {
+  ipcMain.on('query-result-to-main', (_event: void, messagePayload: any) => {
     mainWindow.webContents.send('query-result-to-homepage', messagePayload);
   });
 
-  ipcMain.on('inactivity-logout', (_event, message) => {
+  ipcMain.on('inactivity-logout', (_event: void, _message: string) => {
     mainWindow.webContents.send('inactivity-logout');
   });
 
-  ipcMain.on('logout-reason', (_event, message) => {
+  ipcMain.on('logout-reason', (_event: void, message: string) => {
     mainWindow.webContents.send('logout-reason', message);
+    appDb.set('logoutReason', message);
   });
+
+  mainWindow.on('resize', () => {
+    let { width, height } = mainWindow.getBounds();
+    appDb.set('windowBounds', { width, height }); // getBounds returns an object with the height, width, x and y
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  console.log(`\n\n
+   ('-').->  ('-')               <-.('-')
+  ( OO)_    ( OO).->             __( OO)     <-.
+ (_)--\_)  (,------.  ,------.  '-'---\_)  ,--. )
+ /    _ /   |  .---'  |  .---' |  .-.  |   |  ('-')
+ \_..'--.  (|  '--.   |  '--.  |  | |  |   |  |OO )
+ .-._)   \  |  .--'   |  .--'  |  | |  |  (|  '__ |
+ \       /  |  '---.  |  '---. '  '-'  '-. |     |'
+  '-----'   '------'  '------'  '-----'--' '-----'
+
+              Have a question?
+    Contact us on github.com/oslabs-beta/seeql
+\n\n
+`);
+});
+
+app.on('window-all-closed', () => {
+  // Respect the OSX convention of having the application in memory even
+  // after all windows have been closed
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
