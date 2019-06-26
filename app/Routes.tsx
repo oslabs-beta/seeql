301c985c18d50a4f4a60b3a85cdaa43da274143a
tyler sayles <saylestyler@gmail.com>
2019-06-26 07:32:18 -0400

    this can't possibly be it

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -1,4 +1,11 @@
 import * as React from 'react';
+
+// I'm having an issue using react-router-dom with electron. In development, the
+// router correctly functions and routes users between the different pages. In
+// production, the router no longer functions correctly. In addition to the repo
+// showing the issue, I have some gifs that can quickly outline the problem.
+// Working in development, not working in production.
+
 import { Switch, Route } from 'react-router';
 const routes = require('./constants/routes.json');
 import App from './containers/App';
--- app/index.tsx
+++ app/index.tsx
@@ -4,15 +4,18 @@ import { hot } from 'react-hot-loader/root';
 import ThemeContext from './contexts/themeContext';
 import themes from './themes/themes';
 import Routes from './Routes';
-import { BrowserRouter } from 'react-router-dom';
+// this is react-router-dom, whereas the other components list react-router?
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { useState } from 'react';
+
+import "regenerator-runtime/runtime";
 
 const Index = () => {
   const modes = [
     { value: 'defaultTheme', active: true },
     { value: 'darkTheme', active: false },
-    { currentMode:'defaultTheme' }
+    { currentMode: 'defaultTheme' }
     // { value: 'kateTheme', active: false },
     // { value: 'vaderette', active: false },
     // { value: 'tylerTheme', active: false },
@@ -27,9 +30,9 @@ const Index = () => {
   return (
     <ThemeContext.Provider value={[context, setContext]}>
       <ThemeProvider theme={themes[serveMode]}>
-        <BrowserRouter>
+        <HashRouter>
           <Routes />
-        </BrowserRouter>
+        </HashRouter>
       </ThemeProvider>
     </ThemeContext.Provider>
   );
