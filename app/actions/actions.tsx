41d683f83250d447fc5af776417ffd58f5880c92
Ariel <a.o.hyman@gmail.com>
2020-04-18 23:16:40 -0400

    Add Actions Typescript Interfaces

--- app/actions/actions.tsx
+++ app/actions/actions.tsx
@@ -1,35 +1,5 @@
 import * as actions from '../constants/actionTypes';
 
-//function should take a string and return an object
-
-/*
-export const removeFromPinned = table_name => ({
-  type: actions.REMOVE_FROM_PINNED,
-  payload: {
-    tablename: table_name
-  }
-});
-export const addToPinned = table_name => ({
-    type: actions.ADD_TO_PINNED,
-    payload: {
-        tablename: table_name
-    }});
-
-export const changeToInfoPanel = () => ({
-  type: actions.CHANGE_TO_INFO_PANEL
-});
-
-
-export const changeToFavPanel = () => ({
-  type: actions.CHANGE_TO_FAV_PANEL
-});
-
-export const changeToSettingsPanel = () => ({
-  type: actions.CHANGE_TO_SETTINGS_PANEL
-});
-
-*/
-
 interface TableName {
   tablename: string;
 }
