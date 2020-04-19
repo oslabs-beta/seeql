50ea211aa6f1325df8d87f2db016617eb9a4f1a7
Ariel <a.o.hyman@gmail.com>
2020-04-19 01:02:12 -0400

    Add ChangeDisplaySP Reducer Types

--- app/reducers/ChangeDisplayOfSidePanel.tsx
+++ app/reducers/ChangeDisplayOfSidePanel.tsx
@@ -1,6 +1,25 @@
 import * as types from '../constants/actionTypes';
 
-export default function changedisplayOfSidePanel(state, action) {
+//this object always takes state in the form of an object,and an action in the form of a string
+
+export interface ActionObject {
+  type: string;
+  payload?: Partial<{}>;
+}
+
+type State = Partial<{}>;
+
+/*
+TODO: speciify reducer state properties. Function takes either a state object whos payload's value will be some object with unspecified properties
+
+And return a string or an error object
+
+*/
+
+export default function changedisplayOfSidePanel(
+  _state: State,
+  action: ActionObject
+): string | { new (): Error } {
   switch (action.type) {
     case types.CHANGE_TO_INFO_PANEL:
       return 'info';
