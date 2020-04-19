816c7ed492e4cb98d69d923dcd1d678d1319d9a7
Ariel <a.o.hyman@gmail.com>
2020-04-18 23:48:52 -0400

    Add Context Types

--- app/components/mainpanel/QueryResults.tsx
+++ app/components/mainpanel/QueryResults.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { DataTable } from "grommet";
+import { DataTable } from 'grommet';
 
 const QueryResultWrapper = styled.div`
-      width: 100%;
-    border-radius: 3px;
+  width: 100%;
+  border-radius: 3px;
   overflow: scroll;
   height: 100%;
   padding: 10px 0px 0px 0px;
@@ -20,16 +20,14 @@ const SQueryEmptyState = styled.div`
   font-size: 120%;
   padding: 20px;
   text-align: center;
-
-`
+`;
 
 const SResultsWrapper = styled.div`
   display: flex;
   justify-content: center;
   font-size: 120%;
   overflow: scroll;
-
-`
+`;
 
 interface IQueryResult {
   status: string;
@@ -46,43 +44,51 @@ const QueryResults: React.SFC<IQueryResultsProps> = ({ queryResult }) => {
   if (queryResult.message.length > 0) {
     const columnNames = Object.keys(queryResult.message[0]);
     columnNames.forEach(column => {
-      if (column === 'id') columns.unshift({
-        property: column,
-        header: column,
-      })
-      else columns.push({
-        property: column,
-        header: column,
-      });
+      if (column === 'id')
+        columns.unshift({
+          property: column,
+          header: column
+        });
+      else
+        columns.push({
+          property: column,
+          header: column
+        });
     });
   }
 
   return (
     <QueryResultWrapper>
-      {
-        queryResult.message.length > 0 && (
-          <SResultsWrapper>
-            <DataTable sortable resizable
-              columns={columns.map(c => ({
-                ...c,
-                search: true,
-              }))}
-              data={queryResult.message} step={20} />
-          </SResultsWrapper>
-        )
-      }
-      {
-        queryResult.message.length === 0 &&
-        queryResult.status === 'No results' && (
-          <SQueryEmptyState><div>{`There were no results found for your query.`}<br /> {`Please enter a new query.`}</div></SQueryEmptyState>
-        )
-      }
-      {
-        queryResult.message.length === 0 &&
-        queryResult.status === 'No query' && (
-          <SQueryEmptyState><div>{`You haven't queried anything!`}<br /> {` Enter a query above to get started.`}</div></SQueryEmptyState>
-        )
-      }
+      {queryResult.message.length > 0 && (
+        <SResultsWrapper>
+          <DataTable
+            sortable
+            resizable
+            columns={columns.map(c => ({
+              ...c,
+              search: true
+            }))}
+            data={queryResult.message}
+            step={20}
+          />
+        </SResultsWrapper>
+      )}
+      {queryResult.message.length === 0 && queryResult.status === 'No results' && (
+        <SQueryEmptyState>
+          <div>
+            {`There were no results found for your query.`}
+            <br /> {`Please enter a new query.`}
+          </div>
+        </SQueryEmptyState>
+      )}
+      {queryResult.message.length === 0 && queryResult.status === 'No query' && (
+        <SQueryEmptyState>
+          <div>
+            {`You haven't queried anything!`}
+            <br /> {` Enter a query above to get started.`}
+          </div>
+        </SQueryEmptyState>
+      )}
     </QueryResultWrapper>
   );
 };
--- app/contexts/themeContext.ts
+++ app/contexts/themeContext.ts
@@ -1,5 +1,22 @@
 import { createContext } from 'react';
 
-const ThemeContext = createContext([]);
+/* Partial constructs a type with all properties set to optional.
+
+Partial<{}> becuase explicitly defined interfaces can not be empty. We are returning (using createContext()) an empty array (object) with no default values.
+
+type Partial<T> = { [P in keyof T]?: T[P]; }
+
+- Construct a type, T
+- T will be an object 
+
+- in keyof is an index type query operator that references all the "known" properties of an object. We can explicitly list out the properties that can be valid for the output object HOWEVER, in keyof allows us to automatically update what is required if new properties are added
+
+- P in keyof T references all known properties of arrays
+- ? these properties may or amy not appear on the object
+- T[P] the values of these properties will be the equivalent of accessing the object T at the key P
+
+*/
+
+const ThemeContext: Partial<{}> = createContext([]);
 
 export default ThemeContext;
